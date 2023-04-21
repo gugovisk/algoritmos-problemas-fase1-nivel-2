688a2d9bf30d6163e7f11e5b568c00eb0a2b2f2d
Gustavo Ramos <gu.gugovisk1@gmail.com>
2023-04-21 18:55:20 -0300

    feat: Faz uma busca Linear

--- lib/search.rb
+++ lib/search.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "search/binary"
-
+require_relative "search/linear"
 
 module Search
   class Error < StandardError; end
---
+++ lib/search/linear.rb
@@ -0,0 +1,10 @@
+module Search
+  class Linear
+    def run(array, x)
+      array.each_with_index do |item, index|
+        return index if item.eql? x
+      end
+      -1
+    end
+  end
+end
