30dad3d0b03800f488d9ac7b6f0eba145289bf4b
Gustavo Ramos <gu.gugovisk1@gmail.com>
2023-05-24 20:36:42 -0300

    test(Array): método rassoc

--- spec/texto/missing_chars_spec.rb
+++ spec/texto/missing_chars_spec.rb
@@ -276,4 +276,13 @@ RSpec.describe Texto::Missing_Chars do
       expect(res).to eq([2, "two"])
     end
 
+    it "rassoc - case invalid" do
+      a = [ [ 1, "one"], [2, "two"], [3, "three"], ["ii", "two"] ]
+      res = a.rassoc("four")    #=> nil
+
+      expect(res).to eq(nil)
+    end
+
+
+
 end
