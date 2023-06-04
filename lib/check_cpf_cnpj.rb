4ab25e417f5a7375653758ef92bc29c1c93713e9
Gustavo Ramos <gu.gugovisk1@gmail.com>
2023-06-04 18:22:17 -0300

    feat: check cpu_core

--- lib/check_cpf_cnpj.rb
+++ lib/check_cpf_cnpj.rb
@@ -1,9 +1,18 @@
 require "cpf_cnpj"
+require "os"
 
 class Valida_CPF_CNPJ
 
     def run(cp)
         return CPF.valid?(cp)
+    end
+
+end
+
+class Cpu
+
+    def run(cp)
+        return OS.cpu_count
     end
     
 end
